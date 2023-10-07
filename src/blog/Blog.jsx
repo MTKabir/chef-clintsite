4dbd2b75214bcea02a7180321732df9c3ed39731
kabir <tasnimkabirmd@gmail.com>
2023-10-07 23:59:38 +0200

    footer and blog page done

--- src/blog/Blog.jsx
+++ src/blog/Blog.jsx
@@ -4,8 +4,23 @@ import { useLocation, useNavigate } from 'react-router-dom';
 const Blog = () => {
 
     return (
-        <div>
-            This is blog
+        <div className='row mt-4'>
+            <div className='col-6 border border-info p-2 '>
+                <p className='fw-bold'>Different between controlled and uncontrolled components : </p>
+                <p>In controlled components, we manage it's state using react state management where as in uncontrolled componenet, we don't controll state using react state management rather it is controlled by dom.</p>
+            </div>
+            <div className='col-6 border border-info p-2 '>
+                <p className='fw-bold'>How to validate React props using PropTypes</p>
+                <p>First we have to install npm install prop-types. Then we have to import it. After that we have to validate as follows. For example, if we have a component named PersonalDetails and we want to validate name and age then we can validate like this PersonalDetails.propTypes = name : PropTypes.string.isrequired, age : PropTypes.numbers; </p>
+            </div>
+            <div className='col-6 border border-info p-2 '>
+                <p className='fw-bold'>Different between node.js and express.js</p>
+                <p>Node.js is a runtime environment to run javascript code and express.js is a framework of nodejs.</p>
+            </div>
+            <div className='col-6 border border-info p-2 '>
+                <p className='fw-bold'>What is a custom hook, and why will i create a custom hook?</p>
+                <p>A custom hook is a reuseable JavaScript function. I will use it to make my code more clean and easy to undustandable.</p>
+            </div>
         </div>
     );
 };
--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { SiFacebook, SiInstagram, SiTwitch } from 'react-icons/si';
+import { Link } from 'react-router-dom';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            this is footer
+        <div className='d-flex flex-row justify-content-between mt-4 '>
+            <div>
+                <address>
+                    Level-4, 34, Awal Centre, Banani, Dhaka z<br/>
+                    Support: Web@Programming-Hero.Com<br/>
+                    Available : 10:00am To 07:00pm<br/>
+                </address>
+
+            </div>
+            <div className='d-flex flex-column '>
+                <Link className='text-decoration-none text-black' to="/">Home</Link>
+                <Link className='text-decoration-none text-black' to="/blog">Blog</Link>
+                <Link className='text-decoration-none text-black' to="/login">Login</Link>
+
+            </div>
+            <div>
+                <p>Follow us</p>
+                <div className='d-flex flex-row '>
+                    <SiFacebook className='ms-2 fs-2'></SiFacebook>
+                    <SiTwitch className='ms-2 fs-2'></SiTwitch>
+                    <SiInstagram className='ms-2 fs-2'></SiInstagram>
+                </div>
+            </div>
         </div>
     );
 };
